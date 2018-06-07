8f0ab4b78b991a05ed4979b5e3b0480479af5228
Alex Nichol <unixpickle@gmail.com>
2018-06-07 11:20:15 -0700

    aggregate logging for watch_envs.py

--- master/scripts/watch_envs.py
+++ master/scripts/watch_envs.py
@@ -3,6 +3,7 @@ Monitor activity of all the environments.
 """
 
 import argparse
+import time
 
 import redis
 
@@ -13,11 +14,24 @@ def main():
     conn = redis.StrictRedis(host=args.redis_host, port=args.redis_port)
     pubsub = conn.pubsub()
     pubsub.psubscribe(args.channel + ':state:*')
+    env_list = []
+    sizes = []
+    last_time = time.time()
     for msg in pubsub.listen():
         if msg['type'] != 'pmessage':
             continue
         env_id = str(msg['channel'], 'utf-8').split(':')[-1]
-        print('state from env_id: %s' % env_id)
+        env_list.append(env_id)
+        sizes.append(len(msg['data']))
+        elapsed = time.time() - last_time
+        if elapsed >= args.interval:
+            print('---')
+            print('env_ids: %r' % set(env_list))
+            print('tps: %f' % (len(env_list) / elapsed))
+            print('bytes/sec: %f' % (sum(sizes) / elapsed))
+            env_list = []
+            sizes = []
+            last_time = time.time()
 
 
 def arg_parser():
@@ -25,6 +39,7 @@ def arg_parser():
     parser.add_argument('--redis-host', help='Redis host', default='qwop-redis')
     parser.add_argument('--redis-port', help='Redis port', default=6379, type=int)
     parser.add_argument('--channel', help='channel prefix', default='qwop-worker')
+    parser.add_argument('--interval', help='log interval', default=2, type=float)
     return parser
 
 
